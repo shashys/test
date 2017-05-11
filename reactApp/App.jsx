73371502b200dcfb432cea3ed1aca02f9e44b21f
shashys <shashys@gmail.com>
2017-05-11 18:25:47 +0530

    added test react project

---
+++ reactApp/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import {Header} from './components/header.jsx';
+//import {Header} from './components/header.js';
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+                <div><h1>Header</h1></div>
+              <div><h2>Content</h2></div>
+              <div><h3>Footer</h3></div>
+          </div>
+          
+      );
+   }
+}
+
+export default App;
