bcc435be465daa7b08e47acb707563cd8406dbac
Shashikanth Sarle <shashikanth_sarle@apple.com>
2018-04-02 18:04:59 +0530

    Added main container

--- reactApp/App.jsx
+++ reactApp/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from './components/header.jsx';
+import Main from './components/main.jsx';
 import Footer from './components/footer.jsx';
 class App extends React.Component {
    render() {
       return (
          <div>
-                <Header/>
-              <div><h2>My Content</h2></div>
+              <Header/>
+              <Main/>
               <Footer/>
           </div>
-          
+
       );
    }
 }
--- reactApp/components/main.jsx
+++ reactApp/components/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-class Header extends React.Component {
+class Main extends React.Component {
     render(){
         return(
             <div>
-                <h1>myReact Header</h1>
+                <h1>myReact Main content</h1>
             </div>
         );
     }
 }
 
-export default Header;
+export default Main;
